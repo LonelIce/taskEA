42ce102d903052c5e0a9f9606685381405d6f1a0
Lonel Ice <ijumrz@gmail.com>
2022-04-01 19:24:30 +0300

    Refactor all and update img for README

--- src/timer.js
+++ src/timer.js
@@ -5,8 +5,8 @@ const secondsTimer = document.getElementById('timerSeconds')
 
 const SECONDS_IN_MINUTE = 60;
 const MINUTES_IN_HOUR = 60;
-const MILLISECONDS_IN_SECONDS=1000;
-const HOURS_IN_DAY=24;
+const MILLISECONDS_IN_SECONDS = 1000;
+const HOURS_IN_DAY = 24;
 
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function countdownTimer() {
         const diff = deadline - new Date();
-        const isTimerActive=diff > 0;
+        const isTimerActive = diff > 0;
         if (diff <= 0) {
             clearInterval(timerId);
         }
