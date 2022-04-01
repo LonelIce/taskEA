f273d0d052b918c31d7d629071312560ad21f365
Lonel Ice <ijumrz@gmail.com>
2022-04-01 18:49:44 +0300

    Refactor timer.js

--- src/timer.js
+++ src/timer.js
@@ -3,6 +3,11 @@ const hoursTimer = document.getElementById('timerHours')
 const minutesTimer = document.getElementById('timerMinutes')
 const secondsTimer = document.getElementById('timerSeconds')
 
+const SECONDS_IN_MINUTE = 60;
+const MINUTES_IN_HOUR = 60;
+const MILLISECONDS_IN_SECONDS=1000;
+const HOURS_IN_DAY=24;
+
 document.addEventListener('DOMContentLoaded', function () {
 
     const deadline = new Date(2022, 12, 22);
@@ -11,13 +16,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function countdownTimer() {
         const diff = deadline - new Date();
+        const isTimerActive=diff > 0;
         if (diff <= 0) {
             clearInterval(timerId);
         }
-        const days = '' + (diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0);
-        const hours = '' + (diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0);
-        const minutes = '' + (diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0);
-        const seconds = '' + (diff > 0 ? Math.floor(diff / 1000) % 60 : 0);
+        const days = '' + (isTimerActive ? Math.floor(diff / MILLISECONDS_IN_SECONDS / SECONDS_IN_MINUTE / MINUTES_IN_HOUR / HOURS_IN_DAY) : 0);
+        const hours = '' + (isTimerActive ? Math.floor(diff / MILLISECONDS_IN_SECONDS / SECONDS_IN_MINUTE / MINUTES_IN_HOUR) % HOURS_IN_DAY : 0);
+        const minutes = '' + (isTimerActive ? Math.floor(diff / MILLISECONDS_IN_SECONDS / SECONDS_IN_MINUTE) % MINUTES_IN_HOUR : 0);
+        const seconds = '' + (isTimerActive ? Math.floor(diff / MILLISECONDS_IN_SECONDS) % SECONDS_IN_MINUTE : 0);
 
         dayTimer.innerText = days;
         hoursTimer.innerText = hours;
